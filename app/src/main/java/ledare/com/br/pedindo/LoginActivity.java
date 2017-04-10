fa01d45790fb3f7486ebd21be3a550ce9244e5c1
Silva <mateus.n.silva@accenture.com>
2017-04-10 17:14:12 -0300

    Initial commit

---
+++ app/src/main/java/ledare/com/br/pedindo/LoginActivity.java
@@ -0,0 +1,14 @@
+package ledare.com.br.pedindo;
+
+import android.os.Bundle;
+
+import ledare.com.br.pedindo.activity.BaseActivity;
+
+public class LoginActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
